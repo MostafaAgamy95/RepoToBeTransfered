7f04067b748674474e935e510fb0a2730d69b0aa
MustafaAgamy <soskaagamy1995@gmail.com>
2024-03-20 16:45:42 +0200

    update test class setUp

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,7 +1,13 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class TestClass {
 
     private WebDriver driver;
 
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
 }
